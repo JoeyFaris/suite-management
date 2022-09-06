5a1b823b3972e48d6665606bed7d4b787286bb06
Joey Faris <joeyfaris1@yahoo.com>
2022-09-06 13:46:21 -0700

    requests cards working

--- client/src/components/Maintenance.js
+++ client/src/components/Maintenance.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Maintenance() {
+    const [requests, setRequests] = useState([])
+
+    useEffect(() => {
+fetch("/requests")
+.then((r) => r.json())
+.then((data) => setRequests(data))
+    })
+
+    const renderRequests = requests.map((request) => {
+        return (
+            <div class="flex justify-center">
+  <div class="block rounded-lg shadow-lg bg-white max-w-sm text-center">
+    <div class="p-6">
+      <h5 class="text-gray-900 text-xl font-medium mb-2">{request.category}</h5>
+      <p class="text-gray-700 text-base mb-4">
+        {request.comment}
+      </p>
+      <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+    </div>
+    <div class="py-3 px-6 border-t border-gray-300 text-gray-600">
+      Posted by: {request.user_id.username}
+    </div>
+  </div>
+</div>
+        )
+    })
+
+
+
+    
   return (
-    <div class="absolute ml-80 pl-20 py-20 bg-gray-100">
+    <div class="absolute justify-center ml-72 pl-20 py-20 w-10/12 pr-20 bg-gray-100">
         <div class="text-3xl pb-3">Maintenance Request Form</div>
       <label
         for="countries"
@@ -21,13 +51,14 @@ function Maintenance() {
           <option>Parking</option>
           <option>Patio/Outdoors</option>
         </select>
-        <div class='relative'>
+        <div class=''>
 <label for="message" class="block mb-2 mt-3 text-sm font-medium text-gray-900 dark:text-gray-400">Details</label>
 <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
 </div>
 <button type="button" class="text-white justify-center align-items mt-3 bg-gray-600 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Default</button>
-    </div>
-    
+<div class="grid grid-cols-3 gap-5 py-20 items-start">{renderRequests}</div>
+</div>
+  
   );
 }
 
